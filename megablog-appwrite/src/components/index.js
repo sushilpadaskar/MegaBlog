e65073a1b2518b25b47410061db74276b5bf86b8
sushil <sushilpadaskar07@gmail.com>
2024-10-07 21:55:22 +0530

    made some components for production grade

--- megablog-appwrite/src/components/index.js
+++ megablog-appwrite/src/components/index.js
@@ -1,7 +1,13 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/footer";
+import container from "./container/container";
+import Logo from "./logo";
+import Logoutbtn from "./Header/Logoutbtn";
 
 export {
     Header,
-    Footer
+    Footer,
+    container,
+    Logo,
+    Logoutbtn
 }
